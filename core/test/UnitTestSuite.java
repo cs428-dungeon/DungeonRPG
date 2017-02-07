a09f13af41a4ab0f009fc22daa02db90dba83d7e
Andrew King <luv2bking@gmail.com>
2017-02-07 15:10:01 -0700

    created test directory, added Junit dependency a basic task and a README for creating tests and testsuites

---
+++ core/test/UnitTestSuite.java
@@ -0,0 +1,13 @@
+import edu.byu.rpg.junitTest.JunitUnitTest;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+/**
+ * Created by Andrew on 2/7/2017.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        JunitUnitTest.class
+})
+public class UnitTestSuite {
+    //this is almost always left empty
+}
