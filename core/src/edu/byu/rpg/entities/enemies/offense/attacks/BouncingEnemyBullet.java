6b7c203f9fcc8d821538824f65ff87716589ed08
Andrew King <luv2bking@gmail.com>
2017-03-07 13:04:34 -0700

    Bouncing Bullets now bounce in random directions after colliding with the wall

--- core/src/edu/byu/rpg/entities/enemies/offense/attacks/BouncingEnemyBullet.java
+++ core/src/edu/byu/rpg/entities/enemies/offense/attacks/BouncingEnemyBullet.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.Pool;
 import edu.byu.rpg.RpgGame;
+import edu.byu.rpg.entities.base.Actor;
 import edu.byu.rpg.entities.enemies.offense.base.EnemyAttack;
 import edu.byu.rpg.physics.Body;
 import edu.byu.rpg.physics.Collideable;
@@ -18,13 +19,13 @@ public class BouncingEnemyBullet extends EnemyAttack {
     Texture bulletTexture;
     private float TimeToLive;
     private float maxTimeToLive = 5f;
-    private boolean hitWall = false;
 
     public BouncingEnemyBullet(RpgGame game, World world, Pool<EnemyAttack> pool) {
         super(game, world, new Body(0, 0, 8, 8), pool, World.Type.ENEMY_BULLET);
         bulletTexture = game.assets.getTexture("basic_bullet");
         this.body.maxSpeed = 5.5f;
         this.TimeToLive = maxTimeToLive;
+        body.hitSolid = false;
     }
 
     @Override
@@ -38,7 +39,9 @@ public class BouncingEnemyBullet extends EnemyAttack {
 
         float prevX = body.position.x;
         float prevY = body.position.y;
-        body.updatePosition(delta);
+
+        moveX(delta);
+        moveY(delta);
         TimeToLive -= delta;
         // check for collisions
         if (world.collideCheck(World.Type.PLAYER,body)) {
@@ -53,27 +56,59 @@ public class BouncingEnemyBullet extends EnemyAttack {
                 body.position.x = Utils.approach(body.position.x, prevX, 1);
                 body.position.y = Utils.approach(body.position.y, prevY, 1);
             }
-            hitWall = false;
+            body.hitSolid = false;
             // destroy this bullet
             pop();
         }
-        //redirect the bullet at a new angle.
-        if(world.collideCheck(World.Type.SOLID, body)){
-            float x = -body.velocity.x;
-            float y = -body.velocity.y;
+        if(body.hitSolid == true){
+            float x = (float)Math.random() * 10.0f;
+            x *= (Math.random() > 0.5) ? -1 : 1;
 
-            body.velocity.set(x,y);
+            float y = (float)Math.random() * 10.0f;
+            y *= (Math.random() > 0.5) ? -1 : 1;
 
-            float xAccel = x + 2.0f * x;
-            float yAccel = y + 2.0f * y;
-
-            body.acceleration.set(xAccel, yAccel);
+            body.acceleration.set(x, y);
+            body.velocity.set(0,0);
+            body.hitSolid = false;
         }
         //check if the bullet has been alive for too long, if it has destroy it and reset the timer.
         else if (TimeToLive < 0) {
             pop();
             TimeToLive = maxTimeToLive;
-            hitWall = false;
+            body.hitSolid = false;
+        }
+    }
+    /**
+     * Subroutine of {@link BouncingEnemyBullet#update(float)}, used for moving in the x-direction
+     * without overlapping solids.
+     *
+     * @param delta The time since the last frame.
+     */
+    private void moveX(float delta) {
+        float prevX = body.position.x;
+        body.position.x += body.velocity.x * delta * Body.PIXELS_PER_METER;
+
+        // check for collision, and move back by steps if necessary
+        while (world.collideCheck(World.Type.SOLID, body)) {
+            body.hitSolid = true;
+            body.position.x = Utils.approach(body.position.x, prevX, 1);
+        }
+    }
+
+    /**
+     * Subroutine of {@link BouncingEnemyBullet#update(float)}, used for moving in the y-direction
+     * without overlapping solids.
+     *
+     * @param delta The time since the last frame.
+     */
+    private void moveY(float delta) {
+        float prevY = body.position.y;
+        body.position.y += body.velocity.y * delta * Body.PIXELS_PER_METER;
+
+        // check for collision and move back if necessary
+        while (world.collideCheck(World.Type.SOLID, body)) {
+            body.hitSolid = true;
+            body.position.y = Utils.approach(body.position.y, prevY, 1);
         }
     }
 }
