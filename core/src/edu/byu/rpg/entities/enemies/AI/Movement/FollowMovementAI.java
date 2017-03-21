3316dcb8b404acf6b17cd8e3694ef0994007ba33
Andrew King <luv2bking@gmail.com>
2017-03-21 08:24:38 -0600

    make the radius bigger when scaling the followmovementAI

--- core/src/edu/byu/rpg/entities/enemies/AI/Movement/FollowMovementAI.java
+++ core/src/edu/byu/rpg/entities/enemies/AI/Movement/FollowMovementAI.java
@@ -8,13 +8,15 @@ import edu.byu.rpg.physics.World;
  */
 public class FollowMovementAI implements MovementAI {
     private float movementSpeed = 2.0f;
-    private final float radius = 300.0f;
+    private float radius = 300.0f;
 
     public FollowMovementAI(){
     }
     @Override
     public void scale(float scaleAmount) {
+
         movementSpeed = movementSpeed * scaleAmount;
+        this.radius = radius * scaleAmount * .35f;
     }
 
     @Override
